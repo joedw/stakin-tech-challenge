a29f6d7b3fc8675a5ef99f0df5b56a67fed30638
joedw <j898wdet@protonmail.com>
2021-12-23 17:14:17 +0200

    pew login

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
-            ? 'http://localhost:3000/api' // development api
+            ? 'http://ec2-13-245-2-71.af-south-1.compute.amazonaws.com:3000/api' // development api
             : 'http://localhost:3000/api' // production api
     }
 }
